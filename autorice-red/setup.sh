dcc2ddd9023ccf4fff043d6c42bc1c2a053af22c
Ansh Chauhan <51117244+RiotisWeird@users.noreply.github.com>
2021-10-23 17:45:56 +0530

    Update setup.sh

--- autorice-red/setup.sh
+++ autorice-red/setup.sh
@@ -1,7 +1,9 @@
 #install stuff
-pacman -S vim feh alacritty kitty --no-confirm
+echo "Installing prerequisites"
+pacman -S vim feh alacritty kitty
 
 #Install yay
+echo "Installing yay (AUR Helper)"
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown $USER:`id -gn` ./yay-git
@@ -9,33 +11,40 @@ cd yay-git
 makepkg -si
 
 #install polybar
+echo "Installing polybar"
 yay -S polybar
 
 
 #polybar
+echo "Setting up polybar"
 sh scripts/polybar-themes/polybar.sh
 rm -rf ~/.config/polybar/grayblocks
 cp config/grayblocks ~/.config/polybar
 
 #emojis
+echo "Installing emojis"
 sh scripts/emoji.sh
 
 
 
 #alacritty
+echo "Installing alcritty config"
 mv ~/.config/alacritty/alacritty.yml alacritty.yml.bak
 cp config/alacritty.yml ~/.config/alacritty/alacritty.yml
 mkdir ~/.config/alacritty
 
 #kitty
+echo "Installing kitty config"
 mv ~/.config/kitty/kitty.conf kitty.conf.bak
 cp config/kitty.conf ~/.config/kitty/kitty.conf
 mkdir ~/.config/kitty
 
 #i3-config
+echo "Installing i3 config"
 mv ~/.config/i3/config config.bak
 cp config/i3/config ~/.config/i3/config
 
 #wallpapers
 cp -r wallpapers ~/Pictures
 echo 'exec_always feh --bg-scale ~/Pictures/wallpapers/redarch.png' >> ~/.config/i3/config
+
